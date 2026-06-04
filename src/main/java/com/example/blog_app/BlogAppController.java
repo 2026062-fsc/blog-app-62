ef7ef9c4dec054ac7df51885d72c2257be3f0cdb
K.Matsumoto <2026062@fsc-educ.com>
2026-06-04 14:32:54 +0900

    blogsテンプレートを作成

---
+++ src/main/java/com/example/blog_app/BlogAppController.java
@@ -0,0 +1,13 @@
+package com.example.blog_app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogAppController {
+    @GetMapping("/blogs")
+    public String blogs() {
+        return "blogs";
+    }
+    
+}
